6d9c47de973f4f6913979a5efedcc7bcd7a9f51d
Chris Smith <tophersmith.sec@gmail.com>
2016-01-03 10:37:54 -0500

    continue to configure javadoc generation

--- buildscripts/javadocs_publish.sh
+++ buildscripts/javadocs_publish.sh
@@ -9,20 +9,24 @@ if [ "$TRAVIS_REPO_SLUG" == "tophersmith/Java-Secure-Headers" ] && [ "$TRAVIS_JD
 
   echo -e "Publishing javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-latest
-
+  cp -R security-headers/target/site/apidocs $HOME/javadoc-latest
+  echo -e "Copied javadocs"
+  
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/tophersmith/Java-Secure-Headers gh-pages > /dev/null
-
+  echo -e "Cloned gh-pages"
+  
   cd gh-pages
   git rm -rf ./javadoc
   cp -Rf $HOME/javadoc-latest ./javadoc
   git add -f .
   git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
-
   echo -e "Published Javadoc to gh-pages.\n"
   
   fi
+  
+  
+  
